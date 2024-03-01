d52587f66ddc29559d0729e7b4424920708bb26a
Naman Kharbanda <namankharbanda512@gmail.com>
2024-03-01 21:07:25 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ProgressBar } from "react-bootstrap";
+import { LinearProgress } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  const onClick = () => {
+    for (var i = 0; i <= 100; i++) {
+      setProgress(i);
+      // setTimeout(() => {}, 1000);
+      setInterval(1000);
+    }
+    // setProgress((prev) => prev + 1);
+  };
+
+  // useEffect(() => {
+  //   if (progress > 0) {
+  //     setProgress((prev) => prev + 1);
+  //     console.log(progress);
+  //   }
+  // }, [progress]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "100vh",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            style={{
+              width: "500px",
+              height: "200px",
+              backgroundColor: "#F5F5F5",
+              padding: "20px",
+            }}
+          >
+            <div style={{ display: "flex" }}>
+              <LinearProgress
+                sx={{
+                  borderRadius: "20px",
+                  height: "20px",
+                  marginTop: "10%",
+                  width: "90%",
+                }}
+                variant="determinate"
+                value={progress}
+              />
+              {/* <div>{progress}</div> */}
+            </div>
+
+            <button
+              onClick={onClick}
+              style={{ marginLeft: "40%", marginTop: "10%" }}
+            >
+              Upload
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
